b4ea39fcaf3c641fe4353ce0030d16de6ea167d3
Bilot Tristan <tristan_bilot@carrefour.com>
2020-01-17 14:48:02 +0100

    feat(image): height reducing function

--- Algonos/Modules/Display specific course/CourseElements/CourseElementsFactory.swift
+++ Algonos/Modules/Display specific course/CourseElements/CourseElementsFactory.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class CourseElementsFactory {
   
-  //MARK: - Elements construction
+  //MARK: - Elements building
   func label(text: String) -> UILabel {
       let label = UILabel()
       label.numberOfLines = 0
@@ -19,32 +19,39 @@ class CourseElementsFactory {
       return label
   }
   
-  func image(url: String) -> UIImageView {
+  func image(url: String) -> UIImageView? {
       let image = UIImage().downloadImage(from: url)
       let imageView = UIImageView(image: image)
       imageView.contentMode = .scaleAspectFit
       imageView.translatesAutoresizingMaskIntoConstraints = false
-      /* VALEUR A CHANGER SI JAMAIS BUG SCROLL SUR LA SCROLL VIEW */
-      imageView.heightAnchor.constraint(equalToConstant: (image?.size.height)! / 1.8).isActive = true
+      reduiceHeightOfImage(imageView)
+
       return imageView
   }
   
   func complexity(complexity: Complexity) -> ComplexityView? {
-//    guard let nib = UINib(nibName: "complexityView", bundle: .main)
-//        .instantiate(withOwner: nil, options: nil)[0] as? ComplexityView
-//    guard let nib = Bundle.main.loadNibNamed("complexityView", owner: self, options: nil)?[0] as? ComplexityView
-//        else { return nil }
-//    let nib = ComplexityView.instanceFromNib()
-//      nib.initComplexity(complexity: complexity)
-//    let view = ComplexityView()
-//    let nib = view.instantiateNib()
-//    view.initComplexity(complexity: complexity)
-    
     let customView = ComplexityView.instanceFromNib()
     customView.initComplexity(complexity: complexity)
     customView.translatesAutoresizingMaskIntoConstraints = false
-//    customView.frame = /* add your frame */
-//    view.addSubview(customView)
     return customView
+  }
+  
+  //MARK: - Utils
+  private func reduiceHeightOfImage(_ imageView: UIImageView) {
+    /* We need to reduice the height of the imageView because we don't
+     want to put a static height. So this code reduice the height
+     based on the number of bytes of image. More the image is big,
+     more we need to reduice its height
+     */
+    let bytes = imageView.image?.pngData()?.count ?? 0
+    let imageHeight = imageView.image?.size.height ?? 0
+    var constraintDivisor: CGFloat = 2.0
+    
+    if bytes > 120000 {
+      constraintDivisor = 2.5
+    }
+    
+    let perfectHeight = imageHeight / constraintDivisor
+    imageView.heightAnchor.constraint(equalToConstant: perfectHeight).isActive = true
   }
 }
